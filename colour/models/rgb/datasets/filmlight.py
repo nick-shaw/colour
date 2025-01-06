97aa796c8410221907ebaf21d12701c9d1cafc00
Nick Shaw <nick@antlerpost.com>
2025-01-06 11:09:40 +0000

    Add Filmlight E-Gamut 2

--- colour/models/rgb/datasets/filmlight.py
+++ colour/models/rgb/datasets/filmlight.py
@@ -1,10 +1,11 @@
 """
-FilmLight E-Gamut Colourspace
-=============================
+FilmLight Colourspaces
+======================
 
-Define the *FilmLight E-Gamut* colourspace:
+Define the *FilmLight* colourspaces:
 
 -   :attr:`colour.models.RGB_COLOURSPACE_FILMLIGHT_E_GAMUT`.
+-   :attr:`colour.models.RGB_COLOURSPACE_FILMLIGHT_E_GAMUT_2`.
 
 References
 ----------
@@ -31,7 +32,7 @@ from colour.models.rgb import (
 )
 
 __author__ = "Colour Developers"
-__copyright__ = "Copyright 2013 Colour Developers"
+__copyright__ = "Copyright 2025 Colour Developers"
 __license__ = "BSD-3-Clause - https://opensource.org/licenses/BSD-3-Clause"
 __maintainer__ = "Colour Developers"
 __email__ = "colour-developers@colour-science.org"
@@ -44,6 +45,10 @@ __all__ = [
     "MATRIX_FILMLIGHT_E_GAMUT_TO_XYZ",
     "MATRIX_XYZ_TO_FILMLIGHT_E_GAMUT",
     "RGB_COLOURSPACE_FILMLIGHT_E_GAMUT",
+    "PRIMARIES_FILMLIGHT_E_GAMUT_2",
+    "MATRIX_FILMLIGHT_E_GAMUT_2_TO_XYZ",
+    "MATRIX_XYZ_TO_FILMLIGHT_E_GAMUT_2",
+    "RGB_COLOURSPACE_FILMLIGHT_E_GAMUT_2",
 ]
 
 PRIMARIES_FILMLIGHT_E_GAMUT: NDArrayFloat = np.array(
@@ -85,6 +90,47 @@ RGB_COLOURSPACE_FILMLIGHT_E_GAMUT: RGB_Colourspace = RGB_Colourspace(
 )
 RGB_COLOURSPACE_FILMLIGHT_E_GAMUT.__doc__ = """
 *FilmLight E-Gamut* colourspace.
+
+References
+----------
+:cite:`Siragusano2018a`
+"""
+
+PRIMARIES_FILMLIGHT_E_GAMUT_2: NDArrayFloat = np.array(
+    [
+        [0.8300, 0.3100],
+        [0.1500, 0.9500],
+        [0.0650, -0.0805],
+    ]
+)
+"""*FilmLight E-Gamut 2* colourspace primaries."""
+
+MATRIX_FILMLIGHT_E_GAMUT_2_TO_XYZ: NDArrayFloat = np.array(
+    [
+        [0.736478, 0.130740, 0.083239],
+        [0.275070, 0.828018, -0.103088],
+        [-0.124225, -0.087160, 1.300443]
+    ]
+)
+"""*FilmLight E-Gamut 2* colourspace to *CIE XYZ* tristimulus values matrix."""
+
+MATRIX_XYZ_TO_FILMLIGHT_E_GAMUT_2: NDArrayFloat = np.linalg.inv(
+    MATRIX_FILMLIGHT_E_GAMUT_2_TO_XYZ
+)
+"""*CIE XYZ* tristimulus values to *FilmLight E-Gamut 2* colourspace matrix."""
+
+RGB_COLOURSPACE_FILMLIGHT_E_GAMUT_2: RGB_Colourspace = RGB_Colourspace(
+    "FilmLight E-Gamut 2",
+    PRIMARIES_FILMLIGHT_E_GAMUT_2,
+    CCS_WHITEPOINT_FILMLIGHT_E_GAMUT,
+    WHITEPOINT_NAME_FILMLIGHT_E_GAMUT,
+    MATRIX_FILMLIGHT_E_GAMUT_2_TO_XYZ,
+    MATRIX_XYZ_TO_FILMLIGHT_E_GAMUT_2,
+    log_encoding_FilmLightTLog,
+    log_decoding_FilmLightTLog,
+)
+RGB_COLOURSPACE_FILMLIGHT_E_GAMUT_2.__doc__ = """
+*FilmLight E-Gamut 2* colourspace.
 
 References
 ----------
